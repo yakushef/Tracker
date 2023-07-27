7094057b9645d88233eb0c51a724636fb84b243c
Aleksei Iakushev <av.yakush@gmail.com>
2023-07-27 19:35:21 +0300

    Создан новый проект Xcode

---
+++ Tracker/Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tracker
+//
+//  Created by Aleksey Yakushev on 27.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
