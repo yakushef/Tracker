6b9b27723fc38349b898f73aec0c1deb613af6e3
Aleksei Iakushev <av.yakush@gmail.com>
2023-08-27 13:53:59 +0300

    Добавлены коллекции для выбора эмодзи и цвета при создании нового трекера

---
+++ Tracker/Tracker/Presentation/Create + Edit/EmojiCell.swift
@@ -0,0 +1,39 @@
+//
+//  EmojiCell.swift
+//  Tracker
+//
+//  Created by Aleksey Yakushev on 27.08.2023.
+//
+
+import UIKit
+
+class EmojiCell: UICollectionViewCell {
+    private var emojiCellLabel = UILabel()
+    
+    override var isSelected: Bool {
+        didSet {
+            backgroundColor = isSelected ? .AppColors.lightGray : .clear
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        emojiCellLabel.font = .boldSystemFont(ofSize: 32)
+        addSubview(emojiCellLabel)
+        emojiCellLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            emojiCellLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            emojiCellLabel.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+        layer.cornerRadius = 16
+    }
+    
+    func setEmoji(_ emoji: String) {
+        guard emoji.count == 1 else {
+            emojiCellLabel.text = "🌚"
+            return
+        }
+        emojiCellLabel.text = emoji
+    }
+}
